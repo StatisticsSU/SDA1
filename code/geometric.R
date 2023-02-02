db5eabfb59053db67a6d9891862e5971b7dc2afd
matvil <mattias.villani@stat.su.se>
2023-02-02 15:33:55 +0100

    poisson MLE widget

--- code/geometric.R
+++ code/geometric.R
@@ -6,8 +6,9 @@ library(mosaic)
 # Geometric distribution with p = 0.4
 p = 0.4
 
-# Simulate n = 1000 random numbers from 
-y = rgeom(1000, prob = p) # simulate
+# Simulate n = 10000 random numbers from 
+n = 10000
+y = rgeom(n, prob = p) # simulate
 x = y + 1 # Adding one since R counts the number of failures BEFORE first success
 mean(x) # mean of the simulations, should be close to E(X) = 1/p
 
@@ -20,8 +21,15 @@ pgeom(3, prob = p)
 # Compute 90% quantile 
 qgeom(0.9, prob = p)
 
+# Let's check that mean of the simulated values becomes closer to true E(X) = 1/p 
+# as we add more and more simulated values from our n = 10000 values.
+plot(cumsum(x)/1:n, type = "l", ylab = "mean of simulations", xlab = "number of simulated values")
+abline(a = 1/p, b = 0, col = "red")
+legend("topleft", inset=.01, legend=c("mean", "E(X)=1/p"),
+       col=c("black", "red"), lty = c(1,1), cex=1)
+
 # Plot the true distribution (probabilities) as red dots 
-# Plot the simulated distribution as steelblue bars
+# and the simulated distribution as steelblue bars
 probabilities = data.frame(x = 1:15, y = dgeom(0:14, prob = p)) # Mosaic wants a dataframe
 bargraph(~x, data = as.data.frame(x), type = "proportion", c = "steelblue")
 plotPoints(y ~ x, data =  probabilities, add = T, col = "red", pch = 19) # pch = 19 gives solid dots
