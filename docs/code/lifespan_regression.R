cf5d2cb1fbbc6110a6486f344e33deab6a155b3a
mv <mattias.villani@gmail.com>
2023-03-04 10:23:45 +0100

    changed names to sannolikhetsmodell 1 and 2

--- docs/code/lifespan_regression.R
+++ docs/code/lifespan_regression.R
@@ -5,46 +5,48 @@ library(sda1)
 
 lifespan_no_usa = lifespan[1:29,] # ta bort outliern USA
 
-######################
-# ENKEL REGRESSION 
-######################
+################################
+# ENKEL REGRESSION LIFESPAN 
+################################
 
 # Först enkel regression med bara en förklarande variabel
 model = lm(lifespan ~ spending, data = lifespan_no_usa) 
 summary(model)
 
 # Konfidensintervall - base R
-model = lm(lifespan ~ spending, data = lifespan_no_usa) 
-confint((model))
+confint(model)
 
 # Konfidensintervall - sda1
-model = lm(lifespan ~ spending, data = lifespan_no_usa)
 reg_summary(model, conf_intervals = TRUE, anova = FALSE)
+
+# Residualanalys
+model = lm(lifespan ~ spending, data = lifespan_no_usa)
+reg_residuals(model)
 
 # Prediktionsintervall - sda1
 reg_predict(lifespan ~ spending, data = lifespan_no_usa)
 
-# Prediktion med intervall för ett land som Sverige men med 1 tusen dollar per capita extra 
-
-model = lm(lifespan ~ spending, data = lifespan_no_usa)
+# Anpassat värde/prediktion för Sverige:
+lifespan[26,] # Titt på observationen Sverige
 predict(model, newdata = data.frame(spending = 3.323))
-predict(model, newdata = data.frame(spending = 4.323))
-predict(model, newdata = data.frame(spending = 4.323), interval = "prediction")    
+# Prediktion för ett land som Sverige, men med 1 tusen dollar per capita extra i spending på sjukvård
+predict(model, newdata = data.frame(spending = 4.323)) # så nästan 2 extra levnadsår med de pengarna
+# Prediktion med intervall för ett land som Sverige, men med 1 tusen dollar per capita extra i spending på sjukvård
+predict(model, newdata = data.frame(spending = 4.323), interval = "prediction")   # men det är rätt stor osäkerhet i prediktionen
 
 
 
-######################
-# MULTIPEL REGRESSION 
-######################
+################################
+# MULTIPEL REGRESSION LIFESPAN
+################################
 
 # Multipel regression med tre förklarande variabler
-
 model = lm(lifespan ~ spending + gdp + doctorvisits, data = lifespan_no_usa) # utan USA
 summary(model)
 
 # Prediktion med intervall för ett land som Sverige men med 1 tusen dollar per capita extra 
-
 lifespan[26,]
-predict(model, newdata = data.frame(spending = 3.323, gdp = 40.90968, doctorvisits = 2.8))
 predict(model, newdata = data.frame(spending = 4.323, gdp = 40.90968, doctorvisits = 2.8), 
         interval = "prediction")
+
+
